16f7e081905eaccc8fe15ed93010dab9e9eb4f8c
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-11-12 23:25:35 +0200

    Installed millify library (shortenNumber alt)

--- reddit-client/src/content/Reddit-Posts.js
+++ reddit-client/src/content/Reddit-Posts.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import {
     TiArrowUpOutline,
     TiArrowUpThick,
@@ -8,6 +9,7 @@ import {
     TiMessage,
   } from 'react-icons/ti';
 import { Comment } from "./Comment";
+import shortenNumber from 'shorten-number'
 
 
 export const RedditPosts = (props) => {
@@ -19,15 +21,41 @@ export const RedditPosts = (props) => {
      * @param {number} newValue The new vote value
     */
 
-    const onClickVote = (value) => {
-        if (value === voteValue) {
+    const onClickVote = (newValue) => {
+        if (newValue === voteValue) {
             setVoteValue(0);
-        } else if (value === 1) {
+        } else if (newValue === 1) {
             setVoteValue(1);
         } else {
             setVoteValue(-1);
         }
-    }
+    };
+
+    const renderUpVote = () => {
+        if (voteValue === 1) {
+        return <TiArrowUpThick className="icon-action" />;
+        }
+        return <TiArrowUpOutline className="icon-action" />;
+    };
+    
+    const renderDownVote = () => {
+        if (voteValue === -1) {
+            return <TiArrowDownThick className="icon-action" />;
+        }
+        return <TiArrowDownOutline className="icon-action" />;
+    };
+    
+    const getVoteType = () => {
+        if (voteValue === 1) {
+            return 'up-vote';
+        }
+        if (voteValue === -1) {
+            return 'down-vote';
+        }
+    
+        return '';
+    };
+
 
     const renderComments = () => {
         if (post.errorComments) {
@@ -41,6 +69,7 @@ export const RedditPosts = (props) => {
         if (post.loadingComments) {
             return (
                 <div>
+                    <Skeleton />
                     <Skeleton />
                     <Skeleton />
                     <Skeleton />
@@ -64,11 +93,18 @@ export const RedditPosts = (props) => {
         return (
             <div className="post" key={post.id}>
                 <div className="vote-post">
-                    <p>U</p>
+                    <button className={`icon-action-button up-vote ${voteValue === 1 && 'up-active'}`}
+                    onClick={() => onClickVote(1)}
+                    >
+                        {renderUpVote()}
+                    </button>
                     <br></br>
-                    <p>6969</p>
+                    <p>{shortenNumber(post.ups, 1)}</p>
                     <br></br>
-                    <p>D</p>
+                    <button className={`icon-action-button down-vote ${voteValue === -1 && 'down-active'}`}
+                    onClick={() => onClickVote(-1)}>
+                        {renderDownVote()}
+                    </button>
                 </div>
                 <div className="main-post-txt">
                     <h3>{post.title}</h3>
@@ -85,12 +121,14 @@ export const RedditPosts = (props) => {
                     <div>
                         <button
                             type="button"
-                            className="icon-comment-button"
+                            className={`icon-comment-button ${
+                            post.showingComments && 'showing-comments'
+                            }`}
                             onClick={() => onToggleComments(post.permalink)}
                         >
                             <TiMessage className="icon-comment"/>
                         </button>
-                        <span className="footer-post-comment">464</span>                    
+                        <span className="footer-post-comment">{post.num_comments}</span>                    
                     </div>
                 </div>
                     {renderComments()}
